a1b097d38cb26c556c4a1c813e3b90399743eeab
camilo <camiloesteban661@gmail.com>
2024-08-17 16:24:47 -0500

    feat: add plantuml.nvim and separate alpha config

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,32 +1,6 @@
 ---@type LazySpec
 return {
   {
-    "goolord/alpha-nvim",
-    opts = function(_, opts)
-      opts.section.header.val = {
-        "             \\                                      [            ",
-        "              @                 ⟡                  ╢             ",
-        "      /       ╣▒                                  ]▒       \\     ",
-        "     ╔       ]Ñ▒                                  ╟╣┐       ▓    ",
-        "    ╢╣       ╣▓            √          t            ▓╣       ▓╣   ",
-        "   ▓╣▒╖    ╓╫╜           ╥▓    DOOM   ▓@           ╙▓╖    ╔╣╢║  ",
-        "   ▓▓▓▓  ,p▓,,,,,,      ╜╙▓▄╖,      ,╓╥╜╙╙    ,,,,,,,,▓▓,  ▀▓▓╣U ",
-        "   ▀▓Ö   ╙█▓▓▓▓▓▓╢╫╣▓▓▓▓▓╦, ▀▓▓╗  g╢▓╝ ,╓H╢╢╢╢╢╢▓▓▓▓▓▓▒▓╜   ]▓▓  ",
-        '    ▓▓▓╦╥╖ ╙╙╙╙`     `""▀▓▓@ ▐█▓L]▓╫╛ Æ▒╨╜"       ""╙╙` ╓╖∩▒▒▓   ',
-        ' ╒▓▒╜""╙▀▓▓                ▀  █▒Γ▐▓▓  ╩                ▓╢╜""╙▀█╫L',
-        " ▐▌`      └╝                  ▓▒` █▓                  ╜       └█▓",
-        "▐▓                            ▓▒  █╢                           ▐▓",
-        ' ▐Γ                            ╛  ▐"                           ▐[',
-        " ¬U                                                            jU",
-        "  C                                                            j ",
-        "   L                                                          ]  ",
-      }
-      opts.section.buttons.val = {}
-      opts.section.footer = {}
-      return opts
-    end,
-  },
-  {
     "mattn/emmet-vim",
     lazy = false,
     enabled = false,
